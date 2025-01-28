fcfac6c97000d2116aafef4c14b45576ee20fe90
ivnlsv <ivan.losev@denuo.legal>
2025-01-28 14:05:25 +0300

    upd

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,32 +5,43 @@ const modal = () => {
     
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
-            var opacity = 0;
-            modal.style.display = 'block'
-            modal.style.opacity = opacity
-            var fadein = setInterval(function() {
-                if (opacity < 1) {
-                    opacity += 0.03; // Увеличиваем прозрачность
-                    modal.style.opacity = opacity;
-                } else {
-                    clearInterval(fadein);
-                }
-            }, 50);
+            if (window.innerWidth >= 768) {
+                var opacity = 0;
+                modal.style.display = 'block'
+                modal.style.opacity = opacity
+                var fadein = setInterval(() => {
+                    if (opacity < 1) {
+                        opacity += 0.03; 
+                        modal.style.opacity = opacity;
+                    } else {
+                        clearInterval(fadein);
+                    }
+                }, 50);
+            } else { 
+                modal.style.display = 'block';
+                modal.style.opacity = '1';
+            }
+        
         })
     })
-    closeBtn.addEventListener('click', () => { 
-        var opacity = 1; 
-        modal.style.opacity = opacity;
-    
-        var fadeout = setInterval(function() {
-            if (opacity > 0) {
-                opacity -= 0.03; 
-                modal.style.opacity = opacity;
-            } else {
-                clearInterval(fadeout);
-                modal.style.display = 'none'; 
-            }
-        }, 50); 
+    closeBtn.addEventListener('click', () => {
+        if (window.innerWidth >= 768) {
+            var opacity = 1;
+            modal.style.opacity = opacity;
+            var fadeout = setInterval(() => {
+                if (opacity > 0) {
+                    opacity -= 0.03;
+                    modal.style.opacity = opacity;
+                } else {
+                    clearInterval(fadeout);
+                    modal.style.display = 'none';
+                }
+            }, 50);
+        } else { 
+            modal.style.display = 'none';
+            modal.style.opacity = '0';
+        }
+      
     })
 }
 export default modal
