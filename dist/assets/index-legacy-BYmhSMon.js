f1c24458485bee5bb431d61ae396053278aefcac
ivnlsv <ivan.losev@denuo.legal>
2025-01-28 15:26:17 +0300

    upd

--- dist/assets/index-legacy-BYmhSMon.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent="body{background-color:#0ff}\n/*$vite$:1*/",document.head.appendChild(e)}}}));
