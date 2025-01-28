0c478474c86d38eab209230b544d3bbbf3c2fd0f
ivnlsv <ivan.losev@denuo.legal>
2025-01-28 13:44:20 +0300

    Modal window added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import "./css/bootstrap.min.css";
 import "./css/style.min.css";
 import timer from "./modules/timer";
 import menu from './modules/menu';
+import modal from './modules/modal'
 
 timer("31 january 2025");
 menu();
+modal();
