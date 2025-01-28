9843f09aee2e342742989f07758efb3a563413d5
ivnlsv <ivan.losev@denuo.legal>
2025-01-28 11:18:49 +0300

    update

---
+++ dist/assets/index-legacy-BYmhSMon.js
@@ -0,0 +1,1 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent="body{background-color:#0ff}\n/*$vite$:1*/",document.head.appendChild(e)}}}));
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./css/bootstrap.min.css";
+import "./css/style.min.css";
+import timer from "./modules/timer";
+
+timer("31 january 2025");
