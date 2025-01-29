03357354e66a68b6b7588681ede714ced45398af
ivnlsv <ivan.losev@denuo.legal>
2025-01-29 15:45:37 +0300

    update ref

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,50 +1,39 @@
 const validation = () => {
-    const calcInput = document.querySelectorAll('.calc-item');
-    const textInput = document.querySelectorAll('input[type=text][placeholder="Ваше сообщение"], input[type="text"][placeholder="Ваше имя"]')
-    const emailInput = document.querySelectorAll('input[type=email]') 
-    const telInput = document.querySelectorAll('input[type=tel]') 
+    const inputs = {
+        calcItem: {
+            elements: document.querySelectorAll('.calc-item'),
+            regex: /^[0-9]*$/,
+            errorMsg: 'Введите только цифры'
+        },
+        textItem: {
+            elements: document.querySelectorAll('input[type=text][placeholder="Ваше сообщение"], input[type="text"][placeholder="Ваше имя"]'),
+            regex: /^[а-яА-ЯёЁ\s-]+$/,
+            errorMsg: 'Недопустимый ввод. Введите только буквы на кирилице, пробел или дефис'
+        },
+        emailItem: {
+            elements: document.querySelectorAll('input[type=email]'),
+            regex: /^[a-zA-Z0-9@._!~*'-]+$/,
+            errorMsg: 'Недопустимый ввод. Введите только буквы на латинице, @ - _ . ! ~ *'
+        },
+        telItem: {
+            elements: document.querySelectorAll('input[type=tel]'),
+            regex: /^[0-9()\-]+$/,
+            errorMsg: 'Недопустимый ввод. Введите только цифры 0-9, () - '
+        }
+    };
 
-	calcInput.forEach((input) => {
-		input.addEventListener('input', (e) => {
-			const inputValue = e.target.value;
-			const regex = /^[0-9]*$/;
-			if (!regex.test(inputValue)) {
-				alert('Введите только цифры');
-				e.target.value = '';
-			}
-		});
+    const handleInput = ({ target }, regex, errorMsg) => {
+        if (!regex.test(target.value)) {
+            alert(errorMsg);
+            target.value = '';
+        }
+    };
+
+    Object.values(inputs).forEach(({ elements, regex, errorMsg }) => {
+        elements.forEach(input => {
+            input.addEventListener('input', (e) => handleInput(e, regex, errorMsg));
+        });
     });
-    
-    textInput.forEach(input => { 
-        input.addEventListener('input', (e) => { 
-            const inputValue = e.target.value;
-            const regex = /^[а-яА-ЯёЁ\s-]+$/;
-            if (!regex.test(inputValue)) {
-				alert('Недопустимый ввод. Введите только буквы на кирилице, пробел или дефис');
-				e.target.value = '';
-			}
-        })
-    })
-    emailInput.forEach(input => { 
-        input.addEventListener('input', (e) => { 
-            const inputValue = e.target.value;
-            const regex = /^[a-zA-Z0-9@._!~*'-]+$/;
-            if (!regex.test(inputValue)) {
-				alert('Недопустимый ввод. Введите только буквы на латинице, @ - _ . ! ~ *');
-				e.target.value = '';
-			}
-        })
-    })
-    telInput.forEach(input => { 
-        input.addEventListener('input', (e) => { 
-            const inputValue = e.target.value;
-            const regex = /^[0-9()\-]+$/;
-            if (!regex.test(inputValue)) {
-				alert('Недопустимый ввод. Введите только цифры 0-9, () - ');
-				e.target.value = '';
-			}
-        })
-    })
 };
 
 export default validation;
