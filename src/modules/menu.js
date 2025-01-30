e8a3b5dae8a21a86b1aa238d4e006c46ba21fd38
ivnlsv <ivan.losev@denuo.legal>
2025-01-30 15:43:06 +0300

    upd

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,16 +1,17 @@
 
-const menu = () => { 
-    const menuBtn = document.querySelector('.menu') 
+const menu = () => {
+    const menuBtn = document.querySelector('.menu')
     const menu = document.querySelector('menu')
     const closeBtn = menu.querySelector('.close-btn')
     const menuItems = menu.querySelectorAll('ul>li>a')
 
-    const handleMenu = () => { 
-         menu.classList.toggle('active-menu')
+    const handleMenu = () => {
+        menu.classList.toggle('active-menu')
     }
      
     menuBtn.addEventListener('click', handleMenu)
     closeBtn.addEventListener('click', handleMenu)
+
  
     menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
 
