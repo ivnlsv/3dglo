76cb2a2c07eb251e0a04f8da42b32bf041e44164
ivnlsv <ivan.losev@denuo.legal>
2025-01-30 12:31:50 +0300

    upd

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -43,5 +43,10 @@ const modal = () => {
         }
       
     })
+    modal.addEventListener('click', (e) => { 
+        if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) { 
+            modal.style.display = 'none'
+        }
+    })
 }
 export default modal
