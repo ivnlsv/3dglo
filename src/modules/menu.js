b8cbe5363743a944b284d1a0ae42b01565c54ed3
ivnlsv <ivan.losev@denuo.legal>
2025-01-30 17:40:22 +0300

    upd

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,29 +1,28 @@
-const menu = () => { 
-    const main = document.querySelector('main')
-    const menuBtn = document.querySelector('.menu') 
-    const menu = document.querySelector('menu')
-    const menuItems = menu.querySelectorAll('ul>li>a')
-    
-    const handleMenu = () => { 
-         menu.classList.toggle('active-menu')
-    }
-    
-    main.addEventListener('click', (e) => {
-        if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
-            handleMenu()
-        } else { 
-            handleMenu(menuBtn)
+const menu = () => {
+	const main = document.querySelector('main');
+	const menuBtn = document.querySelector('.menu');
+	const menu = document.querySelector('menu');
+	//const menuItems = menu.querySelectorAll('ul>li>a')
+
+    const toggleMenu = (target) => {
+        // Проверяем, нужно ли открыть или закрыть меню
+        if (target && target !== menuBtn && !menu.contains(target)) {
+            menu.classList.remove('active-menu');
+        } else {
+            menu.classList.toggle('active-menu');
         }
-    }); 
+    }
+
+    main.addEventListener('click', () => {
+        toggleMenu();
+    });
     menu.addEventListener('click', (e) => {
         if (e.target.classList.contains('close-btn')) {
-            handleMenu()
-            
+            toggleMenu(e.target);
         } else if (e.target.matches('ul>li>a')) {
-            menuItems.forEach(menuItem => handleMenu(menuItem))
+            toggleMenu(e.target);
         }
-    })
-    
-    
-}
-export default menu
+    });
+
+};
+export default menu;
