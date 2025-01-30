7516e66bd4bfef614909374bab967abe87ca18ee
ivnlsv <ivan.losev@denuo.legal>
2025-01-30 16:49:15 +0300

    upd

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,29 @@
-
 const menu = () => { 
+    const main = document.querySelector('main')
     const menuBtn = document.querySelector('.menu') 
     const menu = document.querySelector('menu')
-    const closeBtn = menu.querySelector('.close-btn')
     const menuItems = menu.querySelectorAll('ul>li>a')
-
+    
     const handleMenu = () => { 
          menu.classList.toggle('active-menu')
     }
-     
-    menuBtn.addEventListener('click', handleMenu)
-    closeBtn.addEventListener('click', handleMenu)
- 
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
-
+    
+    main.addEventListener('click', (e) => {
+        if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+            handleMenu()
+        } else { 
+            handleMenu(menuBtn)
+        }
+    }); 
+    menu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('close-btn')) {
+            handleMenu()
+            
+        } else if (e.target.matches('ul>li>a')) {
+            menuItems.forEach(menuItem => handleMenu(menuItem))
+        }
+    })
+    
+    
 }
 export default menu
