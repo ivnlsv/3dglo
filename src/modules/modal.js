e854340060c61799765aaa6b1d8c8dd7e46490f1
ivnlsv <ivan.losev@denuo.legal>
2025-02-05 14:11:55 +0300

    upd

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,52 +1,49 @@
-const modal = () => { 
-    const modal = document.querySelector('.popup')
-    const buttons = document.querySelectorAll('.popup-btn')
-    const closeBtn = modal.querySelector('.popup-close')
-    
-    buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            if (window.innerWidth >= 768) {
-                var opacity = 0;
-                modal.style.display = 'block'
-                modal.style.opacity = opacity
-                var fadein = setInterval(() => {
-                    if (opacity < 1) {
-                        opacity += 0.03; 
-                        modal.style.opacity = opacity;
-                    } else {
-                        clearInterval(fadein);
-                    }
-                }, 25);
-            } else { 
-                modal.style.display = 'block';
-                modal.style.opacity = '1';
-            }
-        
-        })
-    })
-    closeBtn.addEventListener('click', () => {
-        if (window.innerWidth >= 768) {
-            var opacity = 1;
-            modal.style.opacity = opacity;
-            var fadeout = setInterval(() => {
-                if (opacity > 0) {
-                    opacity -= 0.03;
-                    modal.style.opacity = opacity;
-                } else {
-                    clearInterval(fadeout);
-                    modal.style.display = 'none';
-                }
-            }, 25);
-        } else { 
-            modal.style.display = 'none';
-            modal.style.opacity = '0';
-        }
-      
-    })
-    modal.addEventListener('click', (e) => { 
-        if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) { 
-            modal.style.display = 'none'
-        }
-    })
-}
-export default modal
+import { animate } from './helpers';
+
+const modal = () => {
+	const modal = document.querySelector('.popup');
+	const buttons = document.querySelectorAll('.popup-btn');
+	const closeBtn = modal.querySelector('.popup-close');
+
+	const showModal = () => {
+		modal.style.display = 'block';
+		animate({
+			timing: (timeFraction) => timeFraction,
+			draw: (progress) => {
+				modal.style.opacity = progress;
+			},
+			duration: 500,
+		});
+	};
+
+	const hideModal = () => {
+		animate({
+			timing: (timeFraction) => timeFraction,
+			draw: (progress) => {
+				modal.style.opacity = 1 - progress;
+			},
+			duration: 500,
+		});
+
+		setTimeout(() => {
+			modal.style.display = 'none';
+		}, 500); //
+	};
+
+	buttons.forEach((btn) => {
+		btn.addEventListener('click', showModal);
+	});
+
+	closeBtn.addEventListener('click', hideModal);
+
+	modal.addEventListener('click', (e) => {
+		if (
+			!e.target.closest('.popup-content') ||
+			e.target.classList.contains('popup-close')
+		) {
+			hideModal();
+		}
+	});
+};
+
+export default modal;
