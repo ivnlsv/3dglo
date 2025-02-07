975017a746e882aaf7d0a3c299f270c7cff3f20d
ivnlsv <78484056+ivnlsv@users.noreply.github.com>
2025-02-07 17:08:14 +0300

    Update helpers.js

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,17 +1,12 @@
 const animate = ({timing, draw, duration}) => {
 
-    let start = performance.now();
+    const start = performance.now();
   
     requestAnimationFrame(function animate(time) {
-      // timeFraction изменяется от 0 до 1
       let timeFraction = (time - start) / duration;
       if (timeFraction > 1) timeFraction = 1;
-  
-      // вычисление текущего состояния анимации
-      let progress = timing(timeFraction);
-  
-      draw(progress); // отрисовать её
-  
+      const progress = timing(timeFraction);
+      draw(progress);
       if (timeFraction < 1) {
         requestAnimationFrame(animate);
       }
