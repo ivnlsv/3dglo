93eab2eacd42508c3bfecae196a595898b9a7c88
ivnlsv <ivan.losev@denuo.legal>
2025-02-10 15:32:58 +0300

    upd

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,6 +1,24 @@
 const sendForm = ({formId, someElem = []}) => { 
      const form =document.getElementById(formId)
+    const statusBlock = document.createElement('div')
+    const loadText = 'Загрузка...'
+    const errorText = 'Ошибка...'
+    const successText = 'Спасибо! Наш менеджер свяжется с вами!'
 
+    const validate = (list) => { 
+         let success = true
+
+        list.forEach(input => { 
+            if (input.classList.contains('success')) { 
+                success = false
+            }
+        })
+        return success 
+
+
+
+    }
+    
     const sendData = (data) => { 
         return fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
@@ -10,25 +28,57 @@ const sendForm = ({formId, someElem = []}) => {
             }
         }).then(res => res.json())
     }
-    
-    form.addEventListener('submit', (e) => { 
-        e.preventDefault()
 
+    const submitForm = () => { 
+        const formElements = document.querySelectorAll('input')
         const formData = new FormData(form)
         const formBody = {}
+
+        statusBlock.textContent = loadText
+        form.append(statusBlock)
 
         formData.forEach((val, key) => { 
             formBody[key] = val
         })
         
-        someElem.forEach(elem => { 
+        someElem.forEach(elem => {
+            const element = document.getElementById(elem.id)
             console.log(elem)
+            if (elem.type === 'block') {
+                formBody[elem.id] = element.textContent
+            } else if (elem.type === 'input') {
+                formBody[elem.id] = element.value
+            }
         })
         console.log('submit')
-        sendData(formBody).then(data => { 
-            console.log(data) 
+        if (validate(formElements)) {
+            sendData(formBody).then(data => {
+                statusBlock.textContent =successText
+                formElements.forEach(input => { 
+                    input.value = ''
+                })
+            })
+                .catch(error => { 
+                    statusBlock.textContent = errorText
+                })
+        } else { 
+            alert('Данные не верны!!!')
+        }
+    }
+    
+    try {
+        if (!form) { 
+            throw new Error ('Верните форму на место, пожалуйста')
+        }
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            
+            submitForm()
+          
         })
-    })
+    } catch (error) { 
+        console.log(error.message)
+    } 
 }
 
 export default sendForm
