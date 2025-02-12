66be6c81195de608ee27fab06d8628b9aacd8c77
ivnlsv <ivan.losev@denuo.legal>
2025-02-12 13:33:29 +0300

    upd

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,83 +1,78 @@
 /* eslint-disable no-unused-vars */
-const sendForm = ({formId, someElem = []}) => { 
-    const form =document.getElementById(formId)
-   const statusBlock = document.createElement('div')
-   const loadText = 'Загрузка...'
-   const errorText = 'Ошибка...'
-   const successText = 'Спасибо! Наш менеджер свяжется с вами!'
+import validation from './validation.js'; 
 
-   const validate = (list) => { 
-        let success = true
+const sendForm = ({ formId, someElem = [] }) => {
+    const form = document.getElementById(formId);
+    const statusBlock = document.createElement('div');
+    const loadText = 'Загрузка...';
+    const errorText = 'Ошибка...';
+    const successText = 'Спасибо! Наш менеджер свяжется с вами!';
 
-       list.forEach(input => { 
-           if (input.classList.contains('success')) { 
-               success = false
-           }
-       })
-       return success 
+    const sendData = (data) => {
+        return fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json"
+            }
+        }).then(res => res.json());
+    };
 
-   }
-   
-   const sendData = (data) => { 
-       return fetch('https://jsonplaceholder.typicode.com/posts', {
-           method: 'POST',
-           body: JSON.stringify(data),
-           headers: {
-               "Content-type": "application/json"
-           }
-       }).then(res => res.json())
-   }
+    const displayErrors = (errors) => {
+        errors.forEach(({ field, message }) => {
+            alert(message); 
+            field.value = '';
+        });
+    };
 
-   const submitForm = () => { 
-       const formElements = document.querySelectorAll('input')
-       const formData = new FormData(form)
-       const formBody = {}
+    const submitForm = () => {
+        const formElements = form.querySelectorAll('input');
+        const formData = new FormData(form);
+        const formBody = {};
+    
+        statusBlock.textContent = loadText;
+        form.append(statusBlock);
 
-       statusBlock.textContent = loadText
-       form.append(statusBlock)
+        formData.forEach((val, key) => {
+            formBody[key] = val;
+        });
 
-       formData.forEach((val, key) => { 
-           formBody[key] = val
-       })
-       
-       someElem.forEach(elem => {
-           const element = document.getElementById(elem.id)
-           console.log(elem)
-           if (elem.type === 'block') {
-               formBody[elem.id] = element.textContent
-           } else if (elem.type === 'input') {
-               formBody[elem.id] = element.value
-           }
-       })
-       console.log('submit')
-       if (validate(formElements)) {
-           sendData(formBody).then(data => {
-               statusBlock.textContent =successText
-               formElements.forEach(input => { 
-                   input.value = ''
-               })
-           })
-               .catch(error => { 
-                   statusBlock.textContent = errorText
-               })
-       } else { 
-           alert('Данные не верны!!!')
-       }
-   }
-   
-   try {
-       if (!form) { 
-           throw new Error ('Верните форму на место, пожалуйста')
-       }
-       form.addEventListener('submit', (e) => {
-           e.preventDefault()
-           
-           submitForm()
-         
-       })
-   } catch (error) { 
-       console.log(error.message)
-   } 
-}
+        someElem.forEach(elem => {
+            const element = document.getElementById(elem.id);
+            if (elem.type === 'block') {
+                statusBlock.style.color = 'white';
+                formBody[elem.id] = element.textContent;
+            } else if (elem.type === 'input') {
+                formBody[elem.id] = element.value;
+            }
+        });
 
-export default sendForm
+        const errors = validation(form); 
+        if (errors.length === 0) {
+            sendData(formBody).then(data => {
+                statusBlock.textContent = successText;
+                formElements.forEach(input => {
+                    input.value = '';
+                });
+            }).catch(error => {
+                statusBlock.textContent = errorText;
+            });
+        } else {
+            displayErrors(errors);
+        }
+    };
+
+    try {
+        if (!form) {
+            throw new Error('Верните форму на место, пожалуйста');
+        }
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            submitForm();
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export default sendForm;
--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,40 +1,38 @@
-/* eslint-disable no-useless-escape */
-const validation = () => {
+const validation = (form) => {
     const inputs = {
         calcItem: {
-            elements: document.querySelectorAll('.calc-item:not(.calc-type)'),
+            elements: form.querySelectorAll('.calc-item:not(.calc-type)'),
             regex: /^[0-9]*$/,
             errorMsg: 'Введите только цифры'
         },
         textItem: {
-            elements: document.querySelectorAll('input[type=text][placeholder="Ваше сообщение"], input[type="text"][placeholder="Ваше имя"]'),
+            elements: form.querySelectorAll('input[type=text][placeholder="Ваше сообщение"], input[type="text"][placeholder="Ваше имя"]'),
             regex: /^[а-яА-ЯёЁ\s-]+$/,
-            errorMsg: 'Недопустимый ввод. Введите только буквы на кирилице, пробел или дефис'
+            errorMsg: 'Недопустимый ввод в текстовое поле. Введите только буквы на кирилице, пробел или дефис'
         },
         emailItem: {
-            elements: document.querySelectorAll('input[type=email]'),
+            elements: form.querySelectorAll('input[type=email]'),
             regex: /^[a-zA-Z0-9@._!~*'-]+$/,
-            errorMsg: 'Недопустимый ввод. Введите только буквы на латинице, @ - _ . ! ~ *'
+            errorMsg: 'Недопустимый ввод e-mail. Введите только буквы на латинице, @ - _ . ! ~ *'
         },
         telItem: {
-            elements: document.querySelectorAll('input[type=tel]'),
-            regex: /^[0-9()\-]+$/,
-            errorMsg: 'Недопустимый ввод. Введите только цифры 0-9, () - '
+            elements: form.querySelectorAll('input[type=tel]'),
+            regex: /^[0-9()\-+]+$/,
+            errorMsg: 'Недопустимый ввод номера телефона. Введите только цифры 0-9, () - +'
         }
     };
 
-    const handleInput = ({ target }, regex, errorMsg) => {
-        if (!regex.test(target.value)) {
-            alert(errorMsg);
-            target.value = '';
-        }
-    };
+    const errors = [];
 
     Object.values(inputs).forEach(({ elements, regex, errorMsg }) => {
         elements.forEach(input => {
-            input.addEventListener('input', (e) => handleInput(e, regex, errorMsg));
+            if (!regex.test(input.value)) {
+                errors.push({ field: input, message: errorMsg });
+            }
         });
     });
+
+    return errors;
 };
 
 export default validation;
